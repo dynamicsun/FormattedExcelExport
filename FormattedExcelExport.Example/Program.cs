b983f48c5d79ec257df48ba61bec1e5327d47343
Mikhail Polelyukhin <mpoleluhin@dunamicsun.ru>
2014-05-27 18:37:53 +0400

    [OPSC-15] Добавил тест для Simple Export

--- FormattedExcelExport.Example/Program.cs
+++ FormattedExcelExport.Example/Program.cs
@@ -9,76 +9,76 @@ using FormattedExcelExport.TableWriters;
 
 
 namespace FormattedExcelExport.Example {
-	class Program {
-		static void Main(string[] args) {
-			var confBuilder = new TableConfigurationBuilder<ClientExampleModel>("Клиент", new CultureInfo("ru-RU"));
-			TableWriterStyle condStyle = new TableWriterStyle();
-			condStyle.RegularCell.BackgroundColor = new AdHocCellStyle.Color(255, 0, 0);
-			TableWriterStyle condStyle2 = new TableWriterStyle();
-			condStyle2.RegularCell.BackgroundColor = new AdHocCellStyle.Color(0, 255, 0);
-			TableWriterStyle condStyle3 = new TableWriterStyle();
-			condStyle3.RegularChildCell.BackgroundColor = new AdHocCellStyle.Color(0, 0, 255);
+    class Program {
+        static void Main(string[] args) {
+            var confBuilder = new TableConfigurationBuilder<ClientExampleModel>("Клиент", new CultureInfo("ru-RU"));
+            TableWriterStyle condStyle = new TableWriterStyle();
+            condStyle.RegularCell.BackgroundColor = new AdHocCellStyle.Color(255, 0, 0);
+            TableWriterStyle condStyle2 = new TableWriterStyle();
+            condStyle2.RegularCell.BackgroundColor = new AdHocCellStyle.Color(0, 255, 0);
+            TableWriterStyle condStyle3 = new TableWriterStyle();
+            condStyle3.RegularChildCell.BackgroundColor = new AdHocCellStyle.Color(0, 0, 255);
 
-			confBuilder.RegisterColumn("Название", x => x.Title, new TableConfigurationBuilder<ClientExampleModel>.ConditionTheme(condStyle, x => x.Title == "Вторая компания"));
-			confBuilder.RegisterColumn("Дата регистрации", x => x.RegistrationDate);
-			confBuilder.RegisterColumn("Телефон", x => x.Phone, new TableConfigurationBuilder<ClientExampleModel>.ConditionTheme(condStyle2, x => x.Okato == "OPEEHBSSDD"));
-			confBuilder.RegisterColumn("ИНН", x => x.Inn);
-			confBuilder.RegisterColumn("Окато", x => x.Okato);
+            confBuilder.RegisterColumn("Название", x => x.Title, new TableConfigurationBuilder<ClientExampleModel>.ConditionTheme(condStyle, x => x.Title == "Вторая компания"));
+            confBuilder.RegisterColumn("Дата регистрации", x => x.RegistrationDate);
+            confBuilder.RegisterColumn("Телефон", x => x.Phone, new TableConfigurationBuilder<ClientExampleModel>.ConditionTheme(condStyle2, x => x.Okato == "OPEEHBSSDD"));
+            confBuilder.RegisterColumn("ИНН", x => x.Inn);
+            confBuilder.RegisterColumn("Окато", x => x.Okato);
 
-			var contact = confBuilder.RegisterChild("Контакт", x => x.Contacts);
-			contact.RegisterColumn("Название", x => x.Title, new TableConfigurationBuilder<ClientExampleModel.Contact>.ConditionTheme(condStyle3, x => x.Title.StartsWith("О")));
-			contact.RegisterColumn("Email", x => x.Email);
+            var contact = confBuilder.RegisterChild("Контакт", x => x.Contacts);
+            contact.RegisterColumn("Название", x => x.Title, new TableConfigurationBuilder<ClientExampleModel.Contact>.ConditionTheme(condStyle3, x => x.Title.StartsWith("О")));
+            contact.RegisterColumn("Email", x => x.Email);
 
-			var contract = confBuilder.RegisterChild("Контракт", x => x.Contracts);
-			contract.RegisterColumn("Дата начала", x => x.BeginDate);
-			contract.RegisterColumn("Дата окончания", x => x.EndDate);
-			contract.RegisterColumn("Статус", x => x.Status, new TableConfigurationBuilder<ClientExampleModel.Contract>.ConditionTheme(new TableWriterStyle(), x => true));
+            var contract = confBuilder.RegisterChild("Контракт", x => x.Contracts);
+            contract.RegisterColumn("Дата начала", x => x.BeginDate);
+            contract.RegisterColumn("Дата окончания", x => x.EndDate);
+            contract.RegisterColumn("Статус", x => x.Status, new TableConfigurationBuilder<ClientExampleModel.Contract>.ConditionTheme(new TableWriterStyle(), x => true));
 
-			var product = confBuilder.RegisterChild("Продукт", x => x.Products);
-			product.RegisterColumn("Наименование", x => x.Title);
-			product.RegisterColumn("Количество", x => x.Amount);
+            var product = confBuilder.RegisterChild("Продукт", x => x.Products);
+            product.RegisterColumn("Наименование", x => x.Title);
+            product.RegisterColumn("Количество", x => x.Amount);
 
-			List<ClientExampleModel> models = InitializeModels();
+            List<ClientExampleModel> models = InitializeModels();
 
-			MemoryStream ms = TableWriterComplex.Write(new DsvTableWriterComplex(), models, confBuilder.Value);
-			WriteToFile(ms, "TestComplex.txt");
-	
-			TableWriterStyle style = new TableWriterStyle();
-			ms = TableWriterComplex.Write(new XlsTableWriterComplex(style), models, confBuilder.Value);
-			WriteToFile(ms, "TestComplex.xls");
+            MemoryStream ms = TableWriterComplex.Write(new DsvTableWriterComplex(), models, confBuilder.Value);
+            WriteToFile(ms, "TestComplex.txt");
 
-			ms = TableWriterSimple.Write(new DsvTableWriterSimple(), models, confBuilder.Value);
-			WriteToFile(ms, "TestSimple.txt");
+            TableWriterStyle style = new TableWriterStyle();
+            ms = TableWriterComplex.Write(new XlsTableWriterComplex(style), models, confBuilder.Value);
+            WriteToFile(ms, "TestComplex.xls");
 
-			ms = TableWriterSimple.Write(new XlsTableWriterSimple(style), models, confBuilder.Value);
-			WriteToFile(ms, "TestSimple.xls");
+            ms = TableWriterSimple.Write(new DsvTableWriterSimple(), models, confBuilder.Value);
+            WriteToFile(ms, "TestSimple.txt");
 
-			TableWriterSimple.Write(new XlsxTableWriterSimple(style), models, confBuilder.Value);
-			TableWriterComplex.Write(new XlsxTableWriterComplex(style), models, confBuilder.Value);
-			
-			ms = ReflectionWriterSimple.Write(models, new DsvTableWriterSimple(), new CultureInfo("ru-Ru"));
-			WriteToFile(ms, "TestReflectionSimple.txt");
+            ms = TableWriterSimple.Write(new XlsTableWriterSimple(style), models, confBuilder.Value);
+            WriteToFile(ms, "TestSimple.xls");
 
-			ms = ReflectionWriterSimple.Write(models, new XlsTableWriterSimple(new TableWriterStyle()), new CultureInfo("ru-Ru"));
-			WriteToFile(ms, "TestReflectionSimple.xls");
+            TableWriterSimple.Write(new XlsxTableWriterSimple(style), models, confBuilder.Value);
+            TableWriterComplex.Write(new XlsxTableWriterComplex(style), models, confBuilder.Value);
 
-			ms = ReflectionWriterComplex.Write(models, new DsvTableWriterComplex(), new CultureInfo("ru-Ru"));
-			WriteToFile(ms, "TestReflectionSimple.txt");
+            ms = ReflectionWriterSimple.Write(models, new DsvTableWriterSimple(), new CultureInfo("ru-Ru"));
+            WriteToFile(ms, "TestReflectionSimple.txt");
 
-			ms = ReflectionWriterComplex.Write(models, new XlsTableWriterComplex(new TableWriterStyle()), new CultureInfo("ru-Ru"));
-			WriteToFile(ms, "TestReflectionComplex.xls");
-		}
+            ms = ReflectionWriterSimple.Write(models, new XlsTableWriterSimple(new TableWriterStyle()), new CultureInfo("ru-Ru"));
+            WriteToFile(ms, "TestReflectionSimple.xls");
 
-		private static void WriteToFile(MemoryStream ms, string fileName) {
-			using (FileStream file = new FileStream(fileName, FileMode.Create, FileAccess.Write)) {
-				byte[] bytes = new byte[ms.Length];
-				ms.Read(bytes, 0, (int)ms.Length);
-				file.Write(bytes, 0, bytes.Length);
-				ms.Close();
-			}
-		}
-		private static List<ClientExampleModel> InitializeModels() {
-			return new List<ClientExampleModel> {
+            ms = ReflectionWriterComplex.Write(models, new DsvTableWriterComplex(), new CultureInfo("ru-Ru"));
+            WriteToFile(ms, "TestReflectionSimple.txt");
+
+            ms = ReflectionWriterComplex.Write(models, new XlsTableWriterComplex(new TableWriterStyle()), new CultureInfo("ru-Ru"));
+            WriteToFile(ms, "TestReflectionComplex.xls");
+        }
+
+        private static void WriteToFile(MemoryStream ms, string fileName) {
+            using (FileStream file = new FileStream(fileName, FileMode.Create, FileAccess.Write)) {
+                byte[] bytes = new byte[ms.Length];
+                ms.Read(bytes, 0, (int)ms.Length);
+                file.Write(bytes, 0, bytes.Length);
+                ms.Close();
+            }
+        }
+        private static List<ClientExampleModel> InitializeModels() {
+            return new List<ClientExampleModel> {
 				new ClientExampleModel(
 					"Первая компания", 
 					DateTime.Now, 
@@ -120,137 +120,137 @@ namespace FormattedExcelExport.Example {
 					},
 					new List<ClientExampleModel.Product> ())		
 			};
-		}
-		[ExcelExportClassName(Name = "Клиент")]
-		public class ClientExampleModel {
-			private readonly string _title;
-			private readonly DateTime _registrationDate;
-			private readonly string _phone;
-			private readonly string _inn;
-			private readonly string _okato;
-			private readonly decimal _revenue;
-			private readonly int _employeeCount;
-			private readonly bool _isActive;
-			private readonly List<Contact> _contacts;
-			private readonly List<Contract> _contracts;
-			private readonly List<Product> _products;
-			public ClientExampleModel(string title, DateTime registrationDate, string phone, string inn, string okato, decimal revenue, int employeeCount, bool isActive, List<Contact> contacts, List<Contract> contracts, List<Product> products) {
-				_title = title;
-				_registrationDate = registrationDate;
-				_phone = phone;
-				_inn = inn;
-				_okato = okato;
-				_revenue = revenue;
-				_employeeCount = employeeCount;
-				_isActive = isActive;
-				_contacts = contacts;
-				_contracts = contracts;
-				_products = products;
-			}
-			[ExcelExport(PropertyName = "Контакты")]
-			public List<Contact> Contacts {
-				get { return _contacts; }
-			}
-			[ExcelExport(PropertyName = "Сделки")]
-			public List<Contract> Contracts {
-				get { return _contracts; }
-			}
-			[ExcelExport(PropertyName = "Продукты")]
-			public List<Product> Products {
-				get { return _products; }
-			}
-			[ExcelExport(PropertyName = "Название")]
-			public string Title {
-				get { return _title; }
-			}
-			[ExcelExport(PropertyName = "Дата регистрации")]
-			public DateTime RegistrationDate {
-				get { return _registrationDate; }
-			}
-			[ExcelExport(PropertyName = "Телефон")]
-			public string Phone {
-				get { return _phone; }
-			}
-			[ExcelExport(PropertyName = "ОКАТО")]
-			public string Okato {
-				get { return _okato; }
-			}
-			[ExcelExport(PropertyName = "ИНН", IsExportable = false)]
-			public string Inn {
-				get { return _inn; }
-			}
-			[ExcelExport(PropertyName = "Прибыль за прошлый год")]
-			public decimal Revenue {
-				get { return _revenue; }
-			}
-			[ExcelExport(IsExportable = false)]
-			public int EmployeeCount {
-				get { return _employeeCount; }
-			}
-			[ExcelExport(PropertyName = "Удалена")]
-			public bool IsActive {
-				get { return _isActive; }
-			}
+        }
+        [ExcelExportClassName(Name = "Клиент")]
+        public class ClientExampleModel {
+            private readonly string _title;
+            private readonly DateTime _registrationDate;
+            private readonly string _phone;
+            private readonly string _inn;
+            private readonly string _okato;
+            private readonly decimal _revenue;
+            private readonly int _employeeCount;
+            private readonly bool _isActive;
+            private readonly List<Contact> _contacts;
+            private readonly List<Contract> _contracts;
+            private readonly List<Product> _products;
+            public ClientExampleModel(string title, DateTime registrationDate, string phone, string inn, string okato, decimal revenue, int employeeCount, bool isActive, List<Contact> contacts, List<Contract> contracts, List<Product> products) {
+                _title = title;
+                _registrationDate = registrationDate;
+                _phone = phone;
+                _inn = inn;
+                _okato = okato;
+                _revenue = revenue;
+                _employeeCount = employeeCount;
+                _isActive = isActive;
+                _contacts = contacts;
+                _contracts = contracts;
+                _products = products;
+            }
+            [ExcelExport(PropertyName = "Контакты")]
+            public List<Contact> Contacts {
+                get { return _contacts; }
+            }
+            [ExcelExport(PropertyName = "Сделки")]
+            public List<Contract> Contracts {
+                get { return _contracts; }
+            }
+            [ExcelExport(PropertyName = "Продукты")]
+            public List<Product> Products {
+                get { return _products; }
+            }
+            [ExcelExport(PropertyName = "Название")]
+            public string Title {
+                get { return _title; }
+            }
+            [ExcelExport(PropertyName = "Дата регистрации")]
+            public DateTime RegistrationDate {
+                get { return _registrationDate; }
+            }
+            [ExcelExport(PropertyName = "Телефон")]
+            public string Phone {
+                get { return _phone; }
+            }
+            [ExcelExport(PropertyName = "ОКАТО")]
+            public string Okato {
+                get { return _okato; }
+            }
+            [ExcelExport(PropertyName = "ИНН", IsExportable = false)]
+            public string Inn {
+                get { return _inn; }
+            }
+            [ExcelExport(PropertyName = "Прибыль за прошлый год")]
+            public decimal Revenue {
+                get { return _revenue; }
+            }
+            [ExcelExport(IsExportable = false)]
+            public int EmployeeCount {
+                get { return _employeeCount; }
+            }
+            [ExcelExport(PropertyName = "Удалена")]
+            public bool IsActive {
+                get { return _isActive; }
+            }
 
-			[ExcelExportClassName(Name = "Контакт")]
-			public sealed class Contact {
-				private readonly string _title;
-				private readonly string _email;
-				public Contact(string title, string email) {
-					_title = title;
-					_email = email;
-				}
-				[ExcelExport(PropertyName = "Название")]
-				public string Title {
-					get { return _title; }
-				}
-				[ExcelExport(PropertyName = "Email")]
-				public string Email {
-					get { return _email; }
-				}
-			}
+            [ExcelExportClassName(Name = "Контакт")]
+            public sealed class Contact {
+                private readonly string _title;
+                private readonly string _email;
+                public Contact(string title, string email) {
+                    _title = title;
+                    _email = email;
+                }
+                [ExcelExport(PropertyName = "Название")]
+                public string Title {
+                    get { return _title; }
+                }
+                [ExcelExport(PropertyName = "Email")]
+                public string Email {
+                    get { return _email; }
+                }
+            }
 
-			[ExcelExportClassName(Name = "Сделка")]
-			public sealed class Contract {
-				private readonly DateTime _beginDate;
-				private readonly DateTime _endDate;
-				private readonly bool _status;
-				public Contract(DateTime beginDate, DateTime endDate, bool status) {
-					_beginDate = beginDate;
-					_endDate = endDate;
-					_status = status;
-				}
-				[ExcelExport(PropertyName = "Дата начала")]
-				public DateTime BeginDate {
-					get { return _beginDate; }
-				}
-				[ExcelExport(PropertyName = "Дата окончания")]
-				public DateTime EndDate {
-					get { return _endDate; }
-				}
-				[ExcelExport(PropertyName = "Статус")]
-				public bool Status {
-					get { return _status; }
-				}
-			}
+            [ExcelExportClassName(Name = "Сделка")]
+            public sealed class Contract {
+                private readonly DateTime _beginDate;
+                private readonly DateTime _endDate;
+                private readonly bool _status;
+                public Contract(DateTime beginDate, DateTime endDate, bool status) {
+                    _beginDate = beginDate;
+                    _endDate = endDate;
+                    _status = status;
+                }
+                [ExcelExport(PropertyName = "Дата начала")]
+                public DateTime BeginDate {
+                    get { return _beginDate; }
+                }
+                [ExcelExport(PropertyName = "Дата окончания")]
+                public DateTime EndDate {
+                    get { return _endDate; }
+                }
+                [ExcelExport(PropertyName = "Статус")]
+                public bool Status {
+                    get { return _status; }
+                }
+            }
 
-			[ExcelExportClassName(Name = "Продукт")]
-			public sealed class Product {
-				private readonly string _title;
-				private readonly int _amount;
-				public Product(string title, int amount) {
-					_title = title;
-					_amount = amount;
-				}
-				[ExcelExport(PropertyName = "Наименование продукта")]
-				public string Title {
-					get { return _title; }
-				}
-				[ExcelExport(PropertyName = "Количество")]
-				public int Amount {
-					get { return _amount; }
-				}
-			}
-		}
-	}
+            [ExcelExportClassName(Name = "Продукт")]
+            public sealed class Product {
+                private readonly string _title;
+                private readonly int _amount;
+                public Product(string title, int amount) {
+                    _title = title;
+                    _amount = amount;
+                }
+                [ExcelExport(PropertyName = "Наименование продукта")]
+                public string Title {
+                    get { return _title; }
+                }
+                [ExcelExport(PropertyName = "Количество")]
+                public int Amount {
+                    get { return _amount; }
+                }
+            }
+        }
+    }
 }
